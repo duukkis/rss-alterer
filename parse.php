d40a9376add4cb270e88e08e2c4b8de8fd5d6e7b
Duukkis <duukkis@hotmail.com>
2015-08-12 08:06:03 +0300

    examples extracted from parse

--- parse.php
+++ parse.php
@@ -83,16 +83,4 @@ function curlFetch($url) {
   return $data;
 }
 
-// examples
 
-// parse namespaced stuff
-writeRss(curlFetch("http://yle.fi/uutiset/rss/uutiset.rss"), "tempx.xml", "Kreikan", "", array("content:encoded"), array(), true, true);
-
-// just get all and write temp.xml
-writeRss(curlFetch("http://www.hs.fi/uutiset/rss/"), "temp.xml", "", "", array(), array(), false, true);
-
-// remove Talous and Ulkomaat
-writeRss(file_get_contents("temp.xml"), "temp_catsremoved.xml", "", "Talous|Ulkomaat", array(), array("category"), false, true);
-
-// leave only stuff with am or pm pattern remove rest
-writeRss(file_get_contents("temp_catsremoved.xml"), "temp_onlyos.xml", "am|pm", "", array("title", "description", "nonexistingfield"), array(), true, true);
